3cd5b645c85ca8f2c929da7fe5fcfb1a31857022
Victor Ruiz~ <vmanr3@gmail.com>
2022-10-23 20:35:11 -0500

    stream builder

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
@@ -19,7 +20,26 @@ class FeedScreen extends StatelessWidget {
         ),
         actions: const [Icon(Icons.messenger_outline)],
       ),
-      body: const PostCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              return PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
