20f8be1aeb50c9b8c7343786ba48263af1287f68
Victor Ruiz~ <vmanr3@gmail.com>
2022-10-23 23:55:43 -0500

    search screen images

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_clone_flutter/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -65,7 +66,29 @@ class _SearchScreenState extends State<SearchScreen> {
                 );
               },
             )
-          : Text('Post'),
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  itemCount: (snapshot.data as dynamic).docs.length,
+                  itemBuilder: (context, index) => Image.network(
+                    (snapshot.data as dynamic).docs[index]['postUrl'],
+                  ),
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                    (index % 7 == 0) ? 2 : 1,
+                    (index % 7 == 0) ? 2 : 1,
+                  ),
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                );
+              },
+            ),
     );
   }
 }
